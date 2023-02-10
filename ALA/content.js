fbec085864a34bdd9eec84cc41df78b39f5825c2
youralien <ryanlouie2021@u.northwestern.edu>
2023-02-09 23:00:09 -0600

    Change name to Rawal, and add F1 and F2 shortcut keys to activate the tips

--- ALA/content.js
+++ ALA/content.js
@@ -16,11 +16,12 @@ function buildingframe() {
 	a.setAttribute("style", "margin-left:5px; margin-right:5px; border: 2px solid Lavender; border-radius: 15px; padding: 8px; cursor: none;");
 
 
-	content = ["Active Listening Assistant (aka ALIA) is active."];
+	content = ["Rawal is active."];
 	// content = ["ALIA is actively viewing what you type, so she can provide helpful tips."];
 	a.innerHTML = '<i>' + content[0] + '</i>';
 
-	var select = document.querySelector("div.c-texty_buttons");
+	// var select = document.querySelector("div.c-texty_buttons");
+	var select = document.querySelector("div.p-message_pane_input_inner_main");
 	select.appendChild(a);
 
 };
@@ -53,7 +54,17 @@ function read_input() {
 				sendrecvData();
 
 			};
-		};
+		} // typing "F1 key" can be a backdoor shortcut to showing feature 1
+		else if(entry.keyCode == 112) {
+			strategy = "Self-disclosure"
+			guide = fix_guide[strategy][0];
+			updatecontent(strategy, guide);
+		} // typing "F2 key" can be a backdoor shortcut to showing feature 1
+		else if(entry.keyCode == 113) {
+			strategy = "Providing Suggestions"
+			guide = fix_guide[strategy][0];
+			updatecontent(strategy, guide);
+		}
 	};
 };
 
@@ -63,11 +74,12 @@ function updatecontent(strategy, guide) {
 	let box = document.querySelector("#agent-block");
 
 	box.removeAttribute('style');
+	// Agent Purple Border Style
 	box.setAttribute("style", "margin-left:5px; margin-right:5px; border: 2px solid #503570; border-radius: 15px; padding: 8px; cursor: help;");
 	box.setAttribute("title", guide); // this is for the hover attribute
 
 	strategyAnnotation = document.createElement("div")
-	strategyAnnotation.innerHTML = "ALIA's annotation: <i>" + strategy + "</i> strategy.</div>";
+	strategyAnnotation.innerHTML = "Active Listening Skill: <i>" + strategy + "</i> </div>";
 
 
 	// Setting up the show and hide button
@@ -100,7 +112,7 @@ function updatecontent(strategy, guide) {
 	// shortTip = "<div><span>Consider reviewing tips on good and bad ways to use this strategy</span>"+toggleButton+"</div>";
 	shortTipContainer = document.createElement("div");
 	shortTip = document.createElement("span")
-	shortTip.innerHTML = "Consider reviewing tips on how to use this strategy";
+	shortTip.innerHTML = "Consider reviewing tips on how to use this skill";
 	shortTipContainer.appendChild(shortTip);
 	shortTip.appendChild(toggleButton);
 
@@ -134,21 +146,21 @@ function gotMessage(message) {
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
-function sendrecvData(){
+var fix_guide = {
+	"Self-disclosure": [
+		"<div>Good: If someone is feeling hopeless, isolated, embarrassed, confused or stuck.</div><div>Bad: If you share an illness story not a wellness or recovery story, or you find yourself with feelings that you are trying to manage while you share.</div>",
+		"If sharing your story seems like it will diminish the member in any way. For example “you think you have it bad, my story is so much worse…” You are seeking to help, not hurt and you never want to “one up” someone.",
+		"If you share an illness story not a wellness or recovery story. The focus needs to be on offering hope to the person you are listening to, not drag anyone down.",
+		"If you find yourself with feelings that you are trying to manage while you share. This is a sign that you may be triggered by the person you are listening to and the focus is then off them and on you. If this is happening, you should exit the conversation and ask the member to find a new listener."
+	],
+	"Providing Suggestions": [
+		"<div>Dos: <ul><li>Show you trust in their ability to work this out, e.g. 'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'</li><li>Help them to think about things from another point of view, e.g. 'What would you say to a friend who was in your situation?'</li><li>Guide them to look inside themselves for the answer, e.g. 'What do you feel like you should do?'</li></ul></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
+		"<div>Dos: If you want to give a suggestion, preface with a question to encourage them to reflect. <i>e.g., 'Have you thought about other ways you could handle this? One idea I had was...'<i></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
+		"<div>Good: Show you trust in their ability to work this out, e.g. <i>'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'<i></div><div>Bad: ",
+	]
+}
 
-	var fix_guide = {
-		"Self-disclosure": [
-			"<div>Good: If someone is feeling hopeless, isolated, embarrassed, confused or stuck.</div><div>Bad: If you share an illness story not a wellness or recovery story, or you find yourself with feelings that you are trying to manage while you share.</div>",
-			"If sharing your story seems like it will diminish the member in any way. For example “you think you have it bad, my story is so much worse…” You are seeking to help, not hurt and you never want to “one up” someone.",
-			"If you share an illness story not a wellness or recovery story. The focus needs to be on offering hope to the person you are listening to, not drag anyone down.",
-			"If you find yourself with feelings that you are trying to manage while you share. This is a sign that you may be triggered by the person you are listening to and the focus is then off them and on you. If this is happening, you should exit the conversation and ask the member to find a new listener."
-		],
-		"Providing Suggestions": [
-			"<div>Dos: <ul><li>Show you trust in their ability to work this out, e.g. 'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'</li><li>Help them to think about things from another point of view, e.g. 'What would you say to a friend who was in your situation?'</li><li>Guide them to look inside themselves for the answer, e.g. 'What do you feel like you should do?'</li></ul></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
-			"<div>Dos: If you want to give a suggestion, preface with a question to encourage them to reflect. <i>e.g., 'Have you thought about other ways you could handle this? One idea I had was...'<i></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
-			"<div>Good: Show you trust in their ability to work this out, e.g. <i>'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'<i></div><div>Bad: ",
-		]
-	}
+function sendrecvData(){
 
 	let text = document.querySelector("#input > p").innerText;
 	console.log(text);
