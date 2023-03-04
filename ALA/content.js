891320d62b65a0afec580897fc4f5ce192b9bbbd
youralien <ryanlouie2021@u.northwestern.edu>
2023-03-04 14:04:11 -0800

    Showing 1 tip followed by 1 suggestion, and connecting it to predictions

--- ALA/content.js
+++ ALA/content.js
@@ -53,12 +53,12 @@ function read_input() {
 		} // typing "F1 key" can be a backdoor shortcut to showing feature 1
 		else if(entry.keyCode == 112) {
 			strategy = "Self-disclosure"
-			guide = fix_guide[strategy][0];
+			guide = fix_guide[strategy];
 			updatecontent(strategy, guide);
 		} // typing "F2 key" can be a backdoor shortcut to showing feature 1
 		else if(entry.keyCode == 113) {
 			strategy = "Providing Suggestions"
-			guide = fix_guide[strategy][0];
+			guide = fix_guide[strategy];
 			updatecontent(strategy, guide);
 		}
 	};
@@ -67,63 +67,21 @@ function read_input() {
 
 function updatecontent(strategy, guide) {
 
-	let box = document.querySelector("#agent-block");
+	// Annotate the strategy
+	strategyAnnotation = document.getElementById("agent-prediction")
+	strategyAnnotation.innerHTML = strategy;
 
-	box.removeAttribute('style');
-	// Agent Purple Border Style
-	box.setAttribute("style", "margin-left:5px; margin-right:5px; border: 2px solid #503570; border-radius: 15px; padding: 8px; cursor: help;");
-	box.setAttribute("title", guide); // this is for the hover attribute
-
-	strategyAnnotation = document.createElement("div")
-	strategyAnnotation.innerHTML = "Active Listening Skill: <i>" + strategy + "</i> </div>";
-
-
-	// Setting up the show and hide button
-	var toggleButton = document.createElement("button");
-	toggleButton.setAttribute("id", "toggleTip");
-	toggleButton.setAttribute("class", "button-3") // green button
-	toggleButton.innerHTML = "Toggle tips";
-	toggleButton.onclick = function() {
-		var x = document.getElementById("verboseTips");
-		if (x.style.display === "none") {
-			x.style.display = "block";
-		} else {
-			x.style.display = "none";
+	// Fill in fixed guides for each tip
+	numSlides = document.getElementsByClassName('slides').tip_slides.childElementCount
+	for (let index = 0; index < numSlides; index++) {
+		slide = document.getElementById(`slide-${index+1}`) // not zero indexed
+		if (index <= guide.length) {
+			slide.innerHTML = guide[index];
 		}
-	};
-
-	document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('toggleTip');
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-				var x = document.getElementById("verboseTips");
-				if (x.style.display === "none") {
-					x.style.display = "block";
-				} else {
-					x.style.display = "none";
-				}
-			});
-	});
-	// shortTip = "<div>ALIA has some tips on using this strategy. <b>*[hover for details]*</b></div>"
-	// shortTip = "<div><span>Consider reviewing tips on good and bad ways to use this strategy</span>"+toggleButton+"</div>";
-	shortTipContainer = document.createElement("div");
-	shortTip = document.createElement("span")
-	shortTip.innerHTML = "Consider reviewing tips on how to use this skill";
-	shortTipContainer.appendChild(shortTip);
-	shortTip.appendChild(toggleButton);
-
-	guideDiv = document.createElement("div");
-	guideDiv.setAttribute("id", "verboseTips")
-	guideDiv.setAttribute("style", "display: none"); // start as hidden;
-	guideDiv.innerHTML = guide;
-
-	// guideDiv = "<div id='verboseTips'>" + guide + "</div>"
-
-	box.appendChild(strategyAnnotation)
-	box.appendChild(shortTip)
-	box.appendChild(guideDiv)
-	// box.innerHTML = strategyAnnotation + shortTip + guideDiv;
-
+		else {
+			slide.innerHTML = `Tip ${index+1}`
+		}
+	}
 
 };
 
@@ -144,15 +102,23 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 var fix_guide = {
 	"Self-disclosure": [
-		"<div>Good: If someone is feeling hopeless, isolated, embarrassed, confused or stuck.</div><div>Bad: If you share an illness story not a wellness or recovery story, or you find yourself with feelings that you are trying to manage while you share.</div>",
-		"If sharing your story seems like it will diminish the member in any way. For example “you think you have it bad, my story is so much worse…” You are seeking to help, not hurt and you never want to “one up” someone.",
-		"If you share an illness story not a wellness or recovery story. The focus needs to be on offering hope to the person you are listening to, not drag anyone down.",
-		"If you find yourself with feelings that you are trying to manage while you share. This is a sign that you may be triggered by the person you are listening to and the focus is then off them and on you. If this is happening, you should exit the conversation and ask the member to find a new listener."
+		"Touch lightly upon your own experience to offer hope and encouragement.",
+		"<i>I struggled with that and I got through it. I know you will, too. There is hope.<i>",
+		"Sharing a small part of your own story can help the person to feel less embarrassed and alone.",
+		"<i>Mary, I hear how painful that is through your words. I also know a bit about how that might feel because I too have been there.</i>",
+		// "<div>Good: If someone is feeling hopeless, isolated, embarrassed, confused or stuck.</div><div>Bad: If you share an illness story not a wellness or recovery story, or you find yourself with feelings that you are trying to manage while you share.</div>",
+		// "If sharing your story seems like it will diminish the member in any way. For example “you think you have it bad, my story is so much worse…” You are seeking to help, not hurt and you never want to “one up” someone.",
+		// "If you share an illness story not a wellness or recovery story. The focus needs to be on offering hope to the person you are listening to, not drag anyone down.",
+		// "If you find yourself with feelings that you are trying to manage while you share. This is a sign that you may be triggered by the person you are listening to and the focus is then off them and on you. If this is happening, you should exit the conversation and ask the member to find a new listener."
 	],
 	"Providing Suggestions": [
-		"<div>Dos: <ul><li>Show you trust in their ability to work this out, e.g. 'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'</li><li>Help them to think about things from another point of view, e.g. 'What would you say to a friend who was in your situation?'</li><li>Guide them to look inside themselves for the answer, e.g. 'What do you feel like you should do?'</li></ul></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
-		"<div>Dos: If you want to give a suggestion, preface with a question to encourage them to reflect. <i>e.g., 'Have you thought about other ways you could handle this? One idea I had was...'<i></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
-		"<div>Good: Show you trust in their ability to work this out, e.g. <i>'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'<i></div><div>Bad: ",
+		"Show you trust in their ability to work this out.",
+		"<i>You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.</i>",
+		"Help them to think about things from another point of view.",
+		"<i>What would you say to a friend who was in your situation?</i>",
+		// "<div>Dos: <ul><li>Show you trust in their ability to work this out, e.g. 'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'</li><li>Help them to think about things from another point of view, e.g. 'What would you say to a friend who was in your situation?'</li><li>Guide them to look inside themselves for the answer, e.g. 'What do you feel like you should do?'</li></ul></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
+		// "<div>Dos: If you want to give a suggestion, preface with a question to encourage them to reflect. <i>e.g., 'Have you thought about other ways you could handle this? One idea I had was...'<i></div><div>Dont's: Don’t judge. Don’t push the person in any direction. Don’t give advice. Don’t give personal opinions.</div>",
+		// "<div>Good: Show you trust in their ability to work this out, e.g. <i>'You are the expert on your situation. I can’t give advice, but I can listen and help you figure out your path forward.'<i></div><div>Bad: ",
 	]
 }
 
@@ -174,13 +140,7 @@ function sendrecvData(){
 		strategy = return_val;
 
 		if (fix_guide[strategy]){
-
-			guide = fix_guide[strategy][0];
-			// console.log(guide + " ----- ");
-			// We are not assuming self-disclosure is bad... since it has some tips
-			// note = strategy + " maybe not effective <b>*[hover for detail]*</b>";
-
-			updatecontent(strategy, guide);
+			updatecontent(strategy, fix_guide[strategy]);
 		};
 
 		console.log("Updatecontent works!!!!");
